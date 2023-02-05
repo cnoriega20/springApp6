1e66c3486fb4d0a90d811c62094ecc9e3908a15f
Cesar Noriega <c79.noriega@gmail.com>
2023-02-05 13:01:24 -0600

    Initial commit

---
+++ src/main/java/com/tn/springapp6/SpringApp6Application.java
@@ -0,0 +1,13 @@
+package com.tn.springapp6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tn/springapp6/SpringApp6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tn.springapp6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApp6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
