1fbc580e1fc771af3c0cf5452a0dafb90127f35f
Cesar Noriega <c79.noriega@gmail.com>
2023-02-25 20:17:49 -0600

    Adding entities

--- src/main/java/com/tn/springapp6/domain/Author.java
+++ src/main/java/com/tn/springapp6/domain/Author.java
@@ -1,4 +1,28 @@
 package com.tn.springapp6.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Author {
+    @Id
+    @Column(name = "idAuthor", nullable = false)
+    private Long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+
 }
--- src/main/java/com/tn/springapp6/domain/Book.java
+++ src/main/java/com/tn/springapp6/domain/Book.java
@@ -1,4 +1,24 @@
 package com.tn.springapp6.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idBook", nullable = false)
+    private Long id;
+
+    @Column(name = "isbn", nullable = false)
+    private String isbn;
+
 }
