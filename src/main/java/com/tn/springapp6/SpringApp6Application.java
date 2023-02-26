87a8a1facb3988f275dc1ca24afc231275c41b0d
Cesar Noriega <c79.noriega@gmail.com>
2023-02-26 17:32:01 -0600

    Added jasypt configuration.

--- src/main/java/com/tn/springapp6/SpringApp6Application.java
+++ src/main/java/com/tn/springapp6/SpringApp6Application.java
@@ -1,8 +1,9 @@
 package com.tn.springapp6;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableEncryptableProperties
 @SpringBootApplication
 public class SpringApp6Application {
 
